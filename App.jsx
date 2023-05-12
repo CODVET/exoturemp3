7ef4e283c736ed7e32b857e690f3a2c1722815ea
CODVET <109933647+CODVET@users.noreply.github.com>
2023-05-12 12:58:58 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,78 @@
+import { useRef, useState } from "react";
+import axios from "axios";
+import { youtube_parser } from "./utils";
+
+function App() {
+  const inputUrlRef = useRef();
+const [urlResult,setUrlResult] = useState(null);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+     const youtubeID = youtube_parser(inputUrlRef.current.value);
+    console.log(youtubeID);
+   
+
+
+    const options = {
+      method: 'get',
+      url: 'https://youtube-mp3-download1.p.rapidapi.com/dl',
+      headers: {
+        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-mp3-download1.p.rapidapi.com'
+      },
+      params: {
+        id: youtubeID
+      }
+
+
+    }
+    axios(options)
+      .then(res => setUrlResult(res.data.link))
+      .catch(err => console.log(err))
+
+    inputUrlRef.current.value = '';
+
+
+  }
+
+  const mystyle = {
+    color: "#0096FF",
+    
+  }
+
+
+  
+
+  return (
+   
+   <div className="app">
+  
+  
+
+    <section className="content">
+      <div className="exoture">Exoture</div>
+      <div className="content-desc"> <u>Secure</u> | <u>Fast</u> | <u>Reliable</u></div>
+      <div className="content-desc2">Past Link, Search, MP3 Appears. -<u>Less Thank 10min</u>- </div>
+
+     <div className="desc3"> Click <u style={mystyle}>DOWNLOAD</u> on Next Page... Then Close It To Return. </div>
+
+
+      <form onSubmit={handleSubmit} className="form" >
+        <input ref={inputUrlRef} className="form-input" placeholder="Paste a Youtube URL Here..." type="tes" />
+        <button target='_blank' type="submit" className="form-button">Search</button>
+
+      </form>
+
+      {urlResult ? <a target='_blank' rel="noreferrer" href={urlResult} className="download-btn"><button className="moving">Take Me To The MP3</button></a> : ''}
+      
+
+    
+    </section>
+    
+  </div>
+  
+  )
+}
+
+export default App
