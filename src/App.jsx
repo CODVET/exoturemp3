2809f05e637c1d3bc5bb75a8c32137227cef53c2
CODVET <109933647+CODVET@users.noreply.github.com>
2023-05-12 12:59:38 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useRef, useState } from "react";
 import axios from "axios";
 import { youtube_parser } from "./utils";
 
-
 function App() {
   const inputUrlRef = useRef();
 const [urlResult,setUrlResult] = useState(null);
@@ -40,35 +39,39 @@ const [urlResult,setUrlResult] = useState(null);
   const mystyle = {
     color: "#0096FF",
     
-    
-    
-
   }
+
+
+  
 
   return (
    
    <div className="app">
   
-    
-      
+  
+
     <section className="content">
       <div className="exoture">Exoture</div>
       <div className="content-desc"> <u>Secure</u> | <u>Fast</u> | <u>Reliable</u></div>
-      <div className="content-desc2"> Click <u style={mystyle}>DOWNLOAD</u> on Next Page... Then Close It To Return.</div>
+      <div className="content-desc2">Past Link, Search, MP3 Appears. -<u>Less Thank 10min</u>- </div>
+
+     <div className="desc3"> Click <u style={mystyle}>DOWNLOAD</u> on Next Page... Then Close It To Return. </div>
 
 
       <form onSubmit={handleSubmit} className="form" >
         <input ref={inputUrlRef} className="form-input" placeholder="Paste a Youtube URL Here..." type="tes" />
-        <button type="submit" className="form-button">Search</button>
+        <button target='_blank' type="submit" className="form-button">Search</button>
 
       </form>
 
-      {urlResult? <a target='_blanck' rel="norefrrer" href={urlResult} className="download-btn"> GET MP3</a> : ''}
+      {urlResult ? <a target='_blank' rel="noreferrer" href={urlResult} className="download-btn"><button className="moving">Take Me To The MP3</button></a> : ''}
       
 
     
     </section>
+    
   </div>
+  
   )
 }
 
